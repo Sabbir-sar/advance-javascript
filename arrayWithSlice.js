440a863c9d7af984dad3d09bb18fe41e1bd32bae
sabbir-sar <sardar.sssm@gmail.com>
2021-03-04 05:51:32 +0600

    pp

---
+++ arrayWithSlice.js
@@ -0,0 +1,25 @@
+/*const nums = [1,2,3,4,5,6,7,8];
+const part = nums.slice(2,5);
+ console.log( part);
+ console.log(nums);*/
+
+
+//splice
+/*const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2,5);
+const remove = nums.splice(2,3);
+ console.log( remove);
+ console.log(nums);*/
+
+ /*const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2,5);
+const remove = nums.splice(2,3, 66,77,95);
+ console.log( remove);
+ console.log(nums);*/
+
+
+ //join
+ const nums = [1,2,3,4,5,6,7,8];
+ // const part = nums.slice(2,5);
+ const together = nums.join('i');
+  console.log( together);
