79129587199d192b622f0da538564b21bc7b856f
sabbir-sar <sardar.sssm@gmail.com>
2021-03-04 05:28:34 +0600

    scope

---
+++ scops.js
@@ -0,0 +1,125 @@
+/*function sum(first,second) {
+    let result = first + second;
+    console.log(result);
+    return result;
+}
+
+const output = sum(3,7);
+ console.log(output)*/
+
+
+/*let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    console.log(result);
+    return result;
+}
+
+const output = sum(3,7);
+console.log(output);
+console.log(bonus);*/
+
+/*let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result>9) {
+        const mood = 'happy';
+        console.log(mood);
+    }
+    return result;
+}
+
+const output = sum(3,7);*/
+// console.log(output);
+// console.log(bonus);
+
+
+/*let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result>9) {
+        let mood = 'happy';
+        mood = "kappy";
+        mood = "jappy";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+const output = sum(3,7);*/
+
+
+/*let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result>9) {
+        var mood = 'happy';
+        mood = "kappy";
+        mood = "jappy";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+const output = sum(3,7);*/
+
+
+//wasting
+/*let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result>9) {
+        var mood = 'happy';
+        mood = "kappy";
+        mood = "jappy";
+        // console.log(mood);
+    }
+    console.log(day);
+    var day = 'friday';
+    return result;
+}
+
+const output = sum(3,7);*/
+
+/*let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result>9) {
+        var mood = 'happy';
+        mood = "kappy";
+        mood = "jappy";
+        // console.log(mood);
+    }
+    
+    var day = 'friday';
+    console.log(day);
+    return result;
+}
+
+const output = sum(3,7);*/
+
+let bonus = 20;
+function sum(first,second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result>9) {
+        var mood = 'happy';
+        mood = "kappy";
+        mood = "jappy";
+        // console.log(mood);
+    }
+    
+    
+    console.log(day);
+    let day = 'friday';
+    return result;
+}
+
+const output = sum(3,7);
