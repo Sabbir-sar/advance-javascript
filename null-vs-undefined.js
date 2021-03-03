5691e25a7569163fa1aaff14a4d0692c4519b2b5
sabbir-sar <sardar.sssm@gmail.com>
2021-03-04 01:58:40 +0600

    doubleQual

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+//undefined value inside function
+/*function add(num1, num2){
+    console.log(num1 + num2);
+    return ;
+}
+const adding = add(23,45);
+console.log(adding);*/
+
+/*function add(num1, num2){
+    console.log(num1, num2);
+  
+}
+const adding = add(23);
+console.log(adding);*/
+
+/*const eat = {food:"beef", price:'I have no money'};
+console.log(eat.water);*/
+
+/*let fun = undefined;
+console.log(fun);*/
+
+let ages = [2,4,7];
+console.log(ages[7]);
